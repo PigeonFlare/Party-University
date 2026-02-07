57c39672d9ea7d3b6ac810870b0337ce560d7d7a
Nandini <nandinim.dharwadkar@gmail.com>
2026-02-06 18:42:11 -0800

    it shud die

--- kill_zone.gd
+++ kill_zone.gd
@@ -1,13 +1,8 @@
 extends Area2D
 
-#@onready var body = $static_body_2d
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	body_entered.connect(_on_body_entered)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_body_entered(body: Node2D) -> void:
 	print("ENTER:", body.name)
-	if body.is_in_group("player"):
-		get_tree().reload_current_scene()
+	get_tree().reload_current_scene()
